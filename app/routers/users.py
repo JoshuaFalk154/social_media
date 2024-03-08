d4cf23df6ac34723d7496d14cdb80492f53f74a5
s445629 <joshua.falk@stud-mail.uni-wuerzburg.de>
2024-03-08 12:50:06 +0100

    create_user finished

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from .. import models, schemas, utils
@@ -7,14 +7,23 @@ from ..database import SessionLocal, engine, get_db
 router = APIRouter()
 
 
-@router.post("/users")
-def get_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    fake_password = utils.get_password_hash(user.password)
-    user.password = fake_password
+@router.post("/users", response_model=schemas.User)
+def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
-    
-    db_user = models.User(**user.model_dump())
-    db.add(db_user)
+    db_user = db.query(models.User).filter_by(email=user.email).first()
+
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail="User already exists!"
+        )
+
+    hashed_password = utils.get_password_hash(user.password)
+    user_data = user.model_dump()
+    user_data.pop("password")
+
+    db_user_create = models.User(**user_data, hashed_password=hashed_password)
+    db.add(db_user_create)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    db.refresh(db_user_create)
+
+    return db_user_create
